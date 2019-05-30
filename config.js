e98107973b7b64e8525d47aa9ccab55592820620
Andre Julius <noromoron@gmail.com>
2019-05-30 22:05:37 +0200

    Add config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    twitchClientId: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    twitchOAuth: "oauth:https://www.youtube.com/watch?v=DLzxrzFCyOs",
+    twitchUserName: "",
+    twitchChannel: "",
+}
